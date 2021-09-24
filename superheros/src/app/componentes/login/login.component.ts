b7f43753ce3544d3e0a446bf1f8465470ed3af87
Javier Alberto Alfaro Jara <javier.alfaro.jara@gmail.com>
2021-09-24 08:45:32 -0600

    checkpoint

--- superheros/src/app/componentes/login/login.component.ts
+++ superheros/src/app/componentes/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var FB: any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,6 +12,41 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    (window as any).fbAsyncInit = function() {
+      FB.init({
+        appId      : '275798627703685',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v12.0'
+      });
+      FB.AppEvents.logPageView();
+    };
+  
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  }
+
+  submitLogin(){
+    console.log("submit login to facebook");
+    // FB.login();
+    FB.login((response)=>
+        {
+          console.log('submitLogin',response);
+          if (response.authResponse)
+          {
+            console.log(response);
+          }
+           else
+           {
+           console.log('User login failed');
+         }
+      });
+
   }
 
 }
